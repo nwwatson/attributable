08ba1110860ede63241143746f3172a5e65e9bba
Nicholas Watson <nwwatson@gmail.com>
2013-05-13 23:27:57 -0400

    added helper for users

--- lib/attributable/engine.rb
+++ lib/attributable/engine.rb
@@ -1,5 +1,11 @@
 module Attributable
   class Engine < ::Rails::Engine
     isolate_namespace Attributable
+
+    def self.activate
+      ActionController::Base.send :include, Attributable::AttributableHelper
+    end
+
+    config.to_prepare &method(:activate).to_proc
   end
 end
