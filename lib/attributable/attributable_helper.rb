acbe23f0bb2d74fb28e8cd4752db129048fa0b39
Nicholas Watson <nick@entropi.co>
2013-12-02 13:47:08 -0500

    wtf

--- lib/attributable/attributable_helper.rb
+++ lib/attributable/attributable_helper.rb
@@ -3,37 +3,42 @@ module Attributable
     extend ActiveSupport::Concern
 
     included do
-      has_many :activities, as: :accountable, class_name: "Attributable::Activity"
+      has_many :activities, as: :trackable, class_name: "Attributable::Activity"
 
-      # Method returns all activities for accountable
+      # Method returns all activities for trackable
       # in chronological order
       def activities_chronologically
         activities.in_order
       end
 
       # Convience method that allows for the quick addition
-      # of an activity for an accountable object. It is required
+      # of an activity for an trackable object. It is required
       # to pass in an action and user. The note is optional
       def add_activity(action, user, note=nil)
         activities << Attributable::Activity({ action: action, trackable: self, user: user, notes: note })
       end
+
+      # Finds the last activity for the object
+      def last_activity
+        activities.order("created_at DESC").last
+      end
     end
 
     # Method returns the class name so that we can use it
-    # in other methods to find accountable items
-    def self.accountable
+    # in other methods to find trackable items
+    def self.trackable
       self.base_class.name
     end
 
-    # Finds all activities for an accountable object
+    # Finds all activities for an trackable object
     def self.find_activities_for(obj)
-      Attributable::Activity.find_activities_for_accountable(accountable, obj.id)
+      Attributable::Activity.find_activities_for_trackable(trackable, obj.id)
     end
 
-    # Finds all activities for an accountable object type
+    # Finds all activities for an trackable object type
     # made by a specific user
     def self.find_activities_by_user(user)
-      Attributable::Activity.find_activities_by_user(user).where(accountable_type: accountable)
+      Attributable::Activity.find_activities_by_user(user).where(trackable: trackable)
     end
   end
 end
